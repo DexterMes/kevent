b8ff179c7b14c366d0ac0b0e9d50402b7a9cb890
DexterMes <kabinbhandari33@gmail.com>
2025-02-01 12:54:47 +0545

    feat: created create event component

---
+++ app/components/CreateEvent.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useState } from "react";
+import CreateEventModal from "./ui/CreateEventModal";
+
+const App: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="rounded-lg bg-blue-600 px-5 py-2.5 text-white hover:bg-blue-700"
+      >
+        Open Modal
+      </button>
+
+      <CreateEventModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </div>
+  );
+};
+
+export default App;
