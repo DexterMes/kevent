4f811472a320c4dc65cab00cb7e556fa35789e6a
DexterMes <kabinbhandari33@gmail.com>
2025-02-01 12:58:49 +0545

    feat: created login and signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+import { DarkThemeToggle } from "flowbite-react";
+import SignUpForm from "../components/SignUpForm";
+
+export default function SignUp() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <SignUpForm />
+    </main>
+  );
+}
